5c56248cff6bf515ce88cec3fd7058ade552fc5c
Marcus Silva <mvsilva1997@gmail.com>
2024-02-14 21:23:07 -0300

    feat: add email confirmation page

---
+++ src/pages/EmailConfirmationPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    <p class="text-body1">
+      Obrigado por se cadastrar!
+    </p>
+    <p class="text-body1">
+      Por favor, confirme seu email para finalizar o seu cadastro: <strong>{{ $route.query.email }}</strong>
+    </p>
+
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'EmailConfirmationPage'
+})
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,8 @@ const routes = [
       { path: '', name: 'loginDefault', component: () => import('pages/LoginPage.vue') },
       { path: 'login', name: 'login', component: () => import('pages/LoginPage.vue') },
       { path: 'register', name: 'register', component: () => import('pages/RegisterPage.vue') },
+      { path: 'email-confirmation', name: 'email-confirmation', component: () => import('pages/EmailConfirmationPage.vue') },
+      { path: 'me', name: 'me', component: () => import('pages/MePage.vue') }
     ]
   },
   {
